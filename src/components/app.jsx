ceacf9bb059c8fceafbf6abd63e8321fac1b5a57
Bruna Guedes <bruna.guedes02@gmail.com>
2021-01-05 11:37:27 -0300

    app component

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="left-scene" />
+        <div className="right-scene" />
+      </div>
+    );
+  }
+}
+
+export default App;
